3efe40831db280081f2bdbb3200de628fe0ad705
nourddine-benyahya <nourddine.benyahya02@gmail.com>
2024-03-01 17:24:36 +0100

    everything is working expect awk or every flag have a single quotes and also norminette it not detected

--- pipex.c
+++ pipex.c
@@ -6,13 +6,23 @@
 /*   By: nbenyahy <nbenyahy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 17:36:06 by nbenyahy          #+#    #+#             */
-/*   Updated: 2024/02/29 20:53:42 by nbenyahy         ###   ########.fr       */
+/*   Updated: 2024/03/01 17:23:26 by nbenyahy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Extra-Library/libft.h"
 #include <stdio.h>
 #include <fcntl.h>
+
+// typedef struct t_list{
+// 	char	**argvcmd;
+// 	char	*path;
+// 	int		pip;
+// 	int		io[2];
+// 	int		i;
+// 	int		pid;
+// 	int		(*fd)[2];
+// } v_list
 
 char	**get_env_path(char *envp[])
 {
@@ -57,33 +67,53 @@ char	*check_cmd_access(char *cmd, char **path)
 	perror("command not faund");
 	return (NULL);
 }
-void	free_2d_arr(char **arr)
+
+void	free_2d_arr(char ***arr)
 {
-	while (*arr != NULL)
+	while (**arr != NULL)
 	{
-		free(*arr);
-		*arr =NULL;
-		arr++;
+		free(**arr);
+		**arr = NULL;
+		(*arr)++;
 	}
+	arr = NULL;
 }
-int	main(int argc, char *argv[], char *envp[])
+
+int	duping(int i, int (*fd)[2], int io[2], int argc)
 {
-	int		i;
-	int		pid;
+	if (i == 2)
+	{
+		dup2(io[0], STDIN_FILENO);
+		dup2(fd[i - 2][1], STDOUT_FILENO);
+		close(fd[i - 2][0]);
+	}
+	else if (i == argc - 2 && argc >= 5)
+	{
+		dup2(fd[i - 3][0], STDIN_FILENO);
+		dup2(io[1], STDOUT_FILENO);
+		close(fd[i - 2][1]);
+		close(fd[i - 2][0]);
+	}
+	else if (i != argc - 2)
+	{
+		dup2(fd[i - 3][0], STDIN_FILENO);
+		dup2(fd[i - 2][1], STDOUT_FILENO);
+		close(fd[i - 2][0]);
+	}
+	return (0);
+}
+
+int execute_command(int argc, char *argv[], char *envp[])
+{
 	char	**argvcmd;
 	char	*path;
-	int		(*fd)[2];
 	int		pip;
 	int		io[2];
+	int		i;
+	int		pid;
+	int		(*fd)[2];
 
-	if (argc < 4)
-		return (ft_printf("arr in nb of args"));
-	if (access(argv[1], F_OK) == -1 || access(argv[argc - 1], F_OK) == -1)
-	{
-		perror("error in io_files (io_file not exist)");
-		return (1);
-	}
-	io[1] = open(argv[argc - 1], O_RDWR);
+	io[1] = open(argv[argc - 1], O_RDWR | O_TRUNC);
 	io[0] = open(argv[1], O_RDWR);
 	i = 2;
 	fd = malloc((argc - 2) * sizeof(int[2]));
@@ -98,35 +128,31 @@ int	main(int argc, char *argv[], char *envp[])
 			perror("pipe error");
 		pid = fork();
 		if (pid == 0) {
-			// for the first cmd
-			if (i == 2 && argc >= 5)
-			{
-				dup2(io[0], STDIN_FILENO);
-				dup2(fd[i - 2][1], STDOUT_FILENO);
-				close(fd[i - 2][0]);
-			}
-			// for the last cmd
-			else if (i == argc - 2 && argc >= 5){
-				close(fd[i - 3][1]);
-				dup2(fd[i - 3][0], STDIN_FILENO);
-				dup2(io[1], STDOUT_FILENO);
-			}
-			// for the middel cmds
-			else if(i != argc - 2)
-			{
-				close(fd[i - 3][1]);
-				dup2(fd[i - 3][0], STDIN_FILENO);
-				dup2(fd[i - 2][1], STDOUT_FILENO);
-			}
+			duping(i, fd, io, argc);
 			if (execve(path, argvcmd, envp) == -1)
 				perror("err in execve");
 		}
-        // close(fd[i - 2][1]);
+		else
+			wait(NULL);
+        close(fd[i - 2][1]);
 		i++;
 		free(path);
 		path = NULL;
-		free_2d_arr(argvcmd);
-		free(argvcmd);
-		argvcmd = NULL;
+		free_2d_arr(&argvcmd);
 	}
+	close(io[0]);
+	close(io[1]);
+	return(0);
+}
+
+int	main(int argc, char *argv[], char *envp[])
+{
+	if (argc < 4)
+		return (ft_printf("arr in nb of args"));
+	if (access(argv[1], F_OK) == -1 || access(argv[argc - 1], F_OK) == -1)
+	{
+		perror("error in io_files (io_file not exist)");
+		return (1);
+	}
+	return (execute_command(argc, argv, envp));
 }
